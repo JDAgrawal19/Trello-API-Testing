1996d12685dba80bdf79019f69b40bff1e37fb13
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-26 18:17:07 +0530

    created boards class and board functions(create,delete)

---
+++ pages/Boards.py
@@ -0,0 +1,25 @@
+import requests
+from utils.constants import *
+from utils.jsondata import *
+
+
+class Boards(object):
+    def __init__(self):
+        self.id = None
+        self.response = None
+
+    def create_a_board_in_organization(self):
+        url = url_create_board
+        self.response = requests.request("POST", url, params=OAUTH_DATA, data=create_board_data)
+        self.id = self.response.json()['id']
+        return self.response
+
+    def delete_a_board_in_oraganization(self):
+        url = url_delete_board.format(id=self.id)
+        response = requests.request("DELETE", url, params=OAUTH_DATA)
+
+    def update_field_of_board_in_organization(self):
+        url = url_update_board_field(id=self.id)
+        response = requests.request("PUT", url, params=OAUTH_DATA)
+
+
