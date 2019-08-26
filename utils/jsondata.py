9bddc7b07915162c29ad61e0885b51a6553ecb26
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-26 14:35:51 +0530

    created test-data file

---
+++ utils/jsondata.py
@@ -0,0 +1,17 @@
+from utils.constants import *
+
+create_org_with_correct_name = {"displayName": "HashedIN", "name":"hashedin_159"}
+
+create_org_incorrect_name_uppercase_letter = {"displayName": "HASHERS", "name": "HASHER546"}
+
+create_org_incorrect_name_special_char = {"displayName": "JDAGRAWAL123", "name": "@#$%^&"}
+
+update_org_display_name = {"displayName": "Jitesh"}
+
+create_member_or_update_type = {"type": "normal"}
+
+get_boards_data = {"filter": "all"}
+
+create_board_data = {"name": "board_1"}
+
+create_list_data = {"name": "list_1", "idBoard": "id"}
