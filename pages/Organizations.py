654ce9814b6dca9f42636b55854e4f8c4c6ba4ac
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-26 15:10:54 +0530

    created function to update fields of Organization

--- pages/Organizations.py
+++ pages/Organizations.py
@@ -17,6 +17,12 @@ class Organizations(object):
         url = url_delete_organization.format(id=self.id)
         requests.request("DELETE", url=url, params=OAUTH_DATA)
 
+    def update_fields_of_organization(self, query):
+        url = url_update_organization.format(id=self.id)
+        self.response = requests.request("PUT", url=url, params=OAUTH_DATA, data=query)
+        return self.response
+
+
 
 
 
