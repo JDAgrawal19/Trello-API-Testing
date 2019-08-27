6cf38c87d624a09db30cb4f63c6cda199dfb6715
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-27 17:22:00 +0530

    added logging to test_boards.py file

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,6 @@
 from utils.jsondata import *
+from utils.custom_logger import custom_logger
+import logging
 from pages.Organizations import Organizations
 from pages.Boards import Boards
 import requests
@@ -7,6 +9,8 @@ import pytest
 
 
 class TestBoard(object):
+    log = custom_logger(logging.ERROR)
+
     @pytest.fixture()
     def setup(self):
         global org
@@ -35,7 +39,10 @@ class TestBoard(object):
         board = Boards()
         create_board_data["idOrganization"] = org.id
         response = board.create_a_board_in_organization(create_board_data)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_create_board")
         board.delete_a_board()
 
     @pytest.mark.usefixtures("setup")
@@ -44,44 +51,70 @@ class TestBoard(object):
         create_board_data["idOrganization"] = org.id
         board.create_a_board_in_organization(create_board_data)
         response = board.delete_a_board()
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_delete_board")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_update_name_of_board(self):
         update_board_name["idOrganization"] = org.id
         response = board.update_fields_of_board(update_board_name)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_update_name_of_board")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_update_board_name_where_name_is_empty(self):
         update_board_name_empty["idOrganization"] = org.id
         response = board.update_fields_of_board(update_board_name_empty)
-        assert response.status_code == requests.codes.bad_request
+        try:
+            assert response.status_code == requests.codes.bad_request
+        except AssertionError:
+            self.log.error("something went wrong with test_update_board_name_where_name_is_empty")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_update_board_name_with_invalid_org_id(self):
         response = board.update_fields_of_board(update_board_name)
-        assert response.status_code == requests.codes.unauthorized
+        try:
+            assert response.status_code == requests.codes.unauthorized
+        except AssertionError:
+            self.log.error("something went wrong with test_update_board_name_with_invalid_org_id")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_add_member_to_board(self):
         response = board.add_member_in_board(member1, data_add_member_in_board)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_add_member_to_board")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_remove_member_present_in_board(self):
         board.add_member_in_board(member1, data_add_member_in_board)
         response = board.remove_member_from_board(member1)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_remove_member_present_in_board")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_try_remove_member_not_present_in_board(self):
         board.add_member_in_board(member1, data_add_member_in_board)
         response = board.remove_member_from_board(member2)
-        assert response.status_code == requests.codes.unauthorized
+        try:
+            assert response.status_code == requests.codes.unauthorized
+        except AssertionError:
+            self.log.error("something went wrong with test_try_remove_member_not_present_in_board")
 
     @pytest.mark.usefixtures("setup_with_board")
     def test_get_members_of_board(self):
         board.add_member_in_board(member1, data_add_member_in_board)
         response = board.get_members_of_board()
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong with test_get_members_of_board")
+
+
