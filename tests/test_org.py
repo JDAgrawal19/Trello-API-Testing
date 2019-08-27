c03d2c19a55aba26db5feaf538154982aa2057d0
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-27 17:11:09 +0530

    added logging to test_org.py file

--- tests/test_org.py
+++ tests/test_org.py
@@ -1,4 +1,6 @@
 from utils.jsondata import *
+from utils.custom_logger import custom_logger
+import logging
 from pages.Organizations import Organizations
 import requests
 import time
@@ -6,6 +8,9 @@ import pytest
 
 
 class TestOrg(object):
+
+    log = custom_logger(logging.DEBUG)
+
     @pytest.fixture()
     def setup(self):
         global org
@@ -26,50 +31,83 @@ class TestOrg(object):
         org = Organizations()
         query = create_org_with_correct_name
         response = org.create_organization(query)
-        assert response.status_code == requests.codes.ok
-        assert response.json()['name'] == query['name']
+        try:
+            assert response.status_code == requests.codes.ok
+            assert response.json()['name'] == query['name']
+        except AssertionError:
+            self.log.error("create organization failed")
         org.delete_organization()
 
     def test_create_organization_incorrect_name_uppercase_letters(self):
         org = Organizations()
         query = create_org_incorrect_name_uppercase_letter
         response = org.create_organization(query)
-        assert response.status_code == requests.codes.ok
-        assert response.json()['name'] == query['name'].lower()
+        try:
+            assert response.status_code == requests.codes.ok
+            assert response.json()['name'] == query['name'].lower()
+        except AssertionError:
+            self.log.error("something went wrong in create_organization_incorrect_name_uppercase ")
         org.delete_organization()
 
     def test_create_organization_incorrect_name_special_chars(self):
         org = Organizations()
         query = create_org_incorrect_name_special_char
         response = org.create_organization(query)
-        assert response.status_code == requests.codes.ok
-        assert response.json()['name'] != query['name']
+        try:
+            assert response.status_code == requests.codes.ok
+            assert response.json()['name'] != query['name']
+        except AssertionError:
+            self.log.error("something went wrong in create_organization_incorrect_name_special_chars")
         org.delete_organization()
 
     @pytest.mark.usefixtures("setup")
     def test_update_org_field_correct_display_name(self):
         query_update = update_org_correct_display_name
         response = org.update_fields_of_organization(query_update)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in create_organization_incorrect_name_special_chars")
 
     @pytest.mark.usefixtures("setup")
     def test_update_org_field_display_name_ends_with_space(self):
         query_update = update_org_incorrect_display_name_ends_with_space
         response_update = org.update_fields_of_organization(query_update)
-        assert response_update.status_code == requests.codes.bad_request
+        try:
+            assert response_update.status_code == requests.codes.bad_request
+        except AssertionError:
+            self.log.error("something went wrong in test_update_org_"
+                           "field_display_name_ends_with_space")
 
     @pytest.mark.usefixtures("setup")
     def test_update_org_field_display_name_empty(self):
         query_update = update_org_incorrect_display_name_empty
         response_update = org.update_fields_of_organization(query_update)
-        assert response_update.status_code == requests.codes.bad_request
+        try:
+            assert response_update.status_code == requests.codes.bad_request
+        except AssertionError:
+            self.log.error("something went wrong in test_update_org_field_display_name_empty")
 
     @pytest.mark.usefixtures("setup")
     def test_add_member_to_org(self):
         query_add_member = create_member_or_update_type
         member = member1
         response = org.add_member_to_organization_or_update_member_type(member, query_add_member)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in test_add_member_to_org")
+
+    @pytest.mark.usefixtures("setup")
+    def test_update_member_type_in_org(self):
+        query_add_member = create_member_or_update_type
+        query_add_member["type"] = "admin"
+        member = member1
+        response = org.add_member_to_organization_or_update_member_type(member, query_add_member)
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in test_update_member_type_in_org")
 
     @pytest.mark.usefixtures("setup")
     def test_remove_member_from_org(self):
@@ -77,18 +115,39 @@ class TestOrg(object):
         member = member1
         org.add_member_to_organization_or_update_member_type(member, query_add_member)
         response = org.remove_member_from_organization(member1)
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in test_remove_member_from_org")
 
     @pytest.mark.usefixtures("setup_create_org")
     def test_delete_organization(self):
         response = org.delete_organization()
-        assert response.status_code == requests.codes.ok
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in test_delete_organization")
 
     @pytest.mark.usefixtures("setup_create_org")
     def test_try_test_delete_organization_twice(self):
         org.delete_organization()
         response = org.delete_organization()
-        assert response.status_code == requests.codes.not_found
+        try:
+            assert response.status_code == requests.codes.not_found
+        except AssertionError:
+            self.log.error("something went wrong in test_try_test_delete_organization_twice")
+
+    @pytest.mark.usefixtures("setup")
+    def test_get_members_of_organization(self):
+        query_add_member = create_member_or_update_type
+        member = member1
+        org.add_member_to_organization_or_update_member_type(member, query_add_member)
+        response = org.get_members_of_organizations()
+        try:
+            assert response.status_code == requests.codes.ok
+        except AssertionError:
+            self.log.error("something went wrong in test_get_members_of_organization")
+
 
 
 
